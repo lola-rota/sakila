64f0d3bcf296d5050511140cb84e0702c0db5c6d
Solepsia <solepsia@gmail.com>
2024-11-27 17:40:10 +0100

    Customer, Film and Rental modules added to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { CustomerModule } from './customer/customer.module';
+import { RentalModule } from './rental/rental.module';
+import { FilmModule } from './film/film.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, CustomerModule, FilmModule, RentalModule],
   controllers: [AppController],
   providers: [AppService],
 })
